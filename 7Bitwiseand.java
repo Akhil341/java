1d9ea04d735c48ac329abe2d850390bf525d6fee
Akhil341 <62087283+Akhil341@users.noreply.github.com>
2020-03-21 18:39:33 +0530

    Add files via upload

---
+++ 7Bitwiseand.java
@@ -0,0 +1,9 @@
+public class Bitwiseand
+{
+	public static void main(String[] args) {
+	   int a=20,b=21,c=a&b;
+	   {
+	   System.out.println(c);
+	   }
+	}
+}
---
+++ 8BitwiseOr.java
@@ -0,0 +1,9 @@
+public class BitwiseOr
+{
+	public static void main(String[] args) {
+	   int a=20,b=21,c=a|b;
+	   {
+	   System.out.println(c);
+	   }
+	}
+}
