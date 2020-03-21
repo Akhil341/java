59fd77d72e2f16cfe2d4287a09d5c6c152bfb13a
Akhil341 <62087283+Akhil341@users.noreply.github.com>
2020-03-21 21:14:26 +0530

    Add files via upload

---
+++ 31ThisKeyword.java
@@ -0,0 +1,13 @@
+public class MyClass 
+{
+  int x;
+  public MyClass(int x)
+ {
+    this.x = x;
+  }
+  public static void main(String[] args)
+ {
+    MyClass myObj = new MyClass(5);
+    System.out.println("Value of x = " + myObj.x);
+  }
+}
---
+++ 34Final.java
@@ -0,0 +1,12 @@
+public class MyClass
+ {
+  final int x = 10;
+
+  public static void main(String[] args)
+ {
+    MyClass myObj = new MyClass();
+    myObj.x = 25; 
+    System.out.println(myObj.x);
+  }
+}
+
